36dc5813ec75e4629711c9d12e40a6b4fedfdb27
Dan Yakovenko <dydev012@gmail.com>
2026-02-08 22:00:03 +0000

    no mot data = never mot

--- bigquery/1_create_main_enriched_view.sql
+++ bigquery/1_create_main_enriched_view.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW `motwot_v2.motwot_main_enriched` AS
 SELECT 
   *,
   ARRAY_LAST(motTests).completedDate AS last_test_date,
-  ARRAY_LAST(motTests).testResult AS last_test_result,
+  COALESCE(ARRAY_LAST(motTests).testResult, 'NEVER MOT') AS last_test_result,
   CAST(ARRAY_LAST(motTests).odometerValue AS INT64) AS mileage,
   DATE_DIFF(CURRENT_DATE(), DATE(firstUsedDate), YEAR) AS vehicle_age,
   (SELECT COUNT(*) FROM UNNEST(motTests) AS t WHERE t.testResult = 'PASSED') AS pass_count,
