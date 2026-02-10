de5e0ee48715d45cdc76fcc4f6372edfc2d8e560
dydev012 <dydev012@gmail.com>
2026-02-10 20:53:51 +0000

    optimise reg lookup

--- looker_studio/reg_lookup.sql
+++ looker_studio/reg_lookup.sql
@@ -1,20 +1,21 @@
-WITH vehicle_data AS (
-  SELECT 
+SELECT
+  * EXCEPT(predicted_last_test_result_probs), 
+  (SELECT prob 
+   FROM UNNEST(predicted_last_test_result_probs) 
+   WHERE label = 'PASSED') AS pass_probability
+
+FROM ML.PREDICT(
+  MODEL `motwot_v2.pass_fail_logreg_model`,
+  (
+    SELECT
+    registration, 
     mileage,
     vehicle_age,
     make,
     model,
     primaryColour
-  FROM `motwot.motwot_v2.motwot_main_enriched`
-  WHERE registration = @reg_input
-  LIMIT 1
+    FROM `motwot_v2.motwot_main_enriched`
+    WHERE registration = @reg_input
+    LIMIT 1
+  )
 )
-
-SELECT
-  p.*,
-  (SELECT prob FROM UNNEST(p.predicted_last_test_result_probs) WHERE label = 'PASSED') AS pass_probability
-FROM vehicle_data vd,
-ML.PREDICT(
-  MODEL `motwot_v2.pass_fail_logreg_model`,
-  (SELECT * FROM vehicle_data)
-) p
