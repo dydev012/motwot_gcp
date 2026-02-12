b1782c13a77fd67410b7b17fcb4ac1eea7418564
dydev012 <dydev012@gmail.com>
2026-02-12 00:13:45 +0000

    train on last 3 yrs

--- bigquery/train_arimap_model.sql
+++ bigquery/train_arimap_model.sql
@@ -9,8 +9,13 @@ OPTIONS(
   data_frequency = 'DAILY',
   clean_spikes_and_dips = TRUE
 ) AS
-SELECT * FROM `motwot_v2.daily_counts`
-WHERE test_date >= '1950-01-01'
+SELECT 
+    DATE(test_date) AS test_date, 
+    result_type,
+    SUM(count) AS count
+FROM `motwot_v2.daily_counts`
+WHERE DATE(test_date) >= DATE_SUB(CURRENT_DATE(), INTERVAL 3 YEAR)
+GROUP BY 1, 2
 
 -- evalute
 -- SELECT * FROM ML.ARIMA_EVALUATE(MODEL `motwot_v2.pass_fail_forecast_model`) 
