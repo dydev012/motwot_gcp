1da7ed5a55a65e829df4868b189a1e7e0cef66de
dydev012 <dydev012@gmail.com>
2026-02-12 00:32:26 +0000

    get route for merge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from fastapi import FastAPI, BackgroundTasks
 from data_runner.data_puller import DataPuller
 from data_runner.data_processor import DataProcessor
 from data_runner.gcp_upload import GCPUploader
@@ -8,10 +9,10 @@ from data_runner._base import DATA_DIR
 DATASET_ID = "motwot_v2"
 TABLE_ID = "motwot_main_enriched"
 
-# daily run job to pull latest delta
+app = FastAPI()
 
 
-def main():
+def perform_weekly_delta_merge():
     # 1 - Pull data
     puller = DataPuller()
     puller.download_deltas()
@@ -20,7 +21,7 @@ def main():
     processor = DataProcessor(data_dir=Path(DATA_DIR))
     filepaths = processor.run()
 
-    # # 3 - Upload data
+    # 3 - Upload data
     uploader = GCPUploader(dataset_id=DATASET_ID, main_table_id=TABLE_ID)
     for i, fp in enumerate(filepaths):
         if i == 0:
@@ -30,5 +31,7 @@ def main():
         os.remove(fp)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/weekly-delta-merge")
+def weekly_delta_merge(background_tasks: BackgroundTasks):
+    background_tasks.add_task(perform_weekly_delta_merge)
+    return {"status": "started"}
